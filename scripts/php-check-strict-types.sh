945ec8f2319e803d9287cbef3826167a99821b5f
daido1976 <daido1976@gmail.com>
2023-05-25 17:12:10 +0900

    Fix script

--- scripts/php-check-strict-types.sh
+++ scripts/php-check-strict-types.sh
@@ -3,7 +3,7 @@
 # ベースブランチと現在の HEAD 間で新規作成されたファイルのみを取得
 new_files=$(git diff --name-status origin/"$BASE_BRANCH"..HEAD | awk '$1 == "A" { print $2 }')
 
-# 新規作成されたPHPファイルに対して PHP-CS-Fixer を実行
+# 新規作成されたPHPファイルがdeclare(strict_types=1);を含んでいるか確認
 for file in $new_files; do
     # テストファイルはチェックをスキップ
     if [[ $file == *Test.php ]]; then
@@ -11,6 +11,9 @@ for file in $new_files; do
     fi
 
     if [[ $file == *.php ]]; then
-        php-cs-fixer fix --rules=declare_strict_types "$file"
+        if ! grep -qs '^declare(strict_types=1);' "$file"; then
+            echo "Error: New PHP file $file does not start with 'declare(strict_types=1);'."
+            exit 1
+        fi
     fi
 done
